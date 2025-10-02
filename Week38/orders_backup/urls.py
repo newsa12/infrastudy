7d51af6c9f57b1b8dba9e708944338e7f8f088ae
newsa12 <pcy5748@gmail.com>
2025-10-02 14:45:12 +0900

    fix: .gitignore 적용 및 불필요한 파일 추적 해제

--- Week38/orders_backup/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import OrderListCreate, OrderDetail
-
-urlpatterns = [
-    path("api/v1/orders", OrderListCreate.as_view(), name="order-list-create"),
-    path("api/v1/orders/<int:order_id>", OrderDetail.as_view(), name="order-detail"),
-]
